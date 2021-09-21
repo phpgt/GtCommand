bedd0a564e58939d1ffe9747b627ce38df32b421
Greg Bowler <greg.bowler@g105b.com>
2021-09-21 17:27:02 +0100

    feature: add command classes to implement

---
+++ src/Command/BuildCommand.php
@@ -0,0 +1,49 @@
+<?php
+namespace Gt\GtCommand\Command;
+
+use Gt\Cli\Argument\ArgumentValueList;
+use Gt\Cli\Command\Command;
+use Gt\Cli\Parameter\NamedParameter;
+use Gt\Cli\Parameter\Parameter;
+
+class BuildCommand extends Command {
+	public function run(ArgumentValueList $arguments = null):void {
+		// TODO: Implement run() method.
+	}
+
+	public function getName():string {
+		// TODO: Implement getName() method.
+	}
+
+	public function getDescription():string {
+		// TODO: Implement getDescription() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRequiredNamedParameterList():array {
+		// TODO: Implement getRequiredNamedParameterList() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getOptionalNamedParameterList():array {
+		// TODO: Implement getOptionalNamedParameterList() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRequiredParameterList():array {
+		// TODO: Implement getRequiredParameterList() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getOptionalParameterList():array {
+		// TODO: Implement getOptionalParameterList() method.
+	}
+}
