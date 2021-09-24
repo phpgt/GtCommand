8db3edfe64b02b812b667a7d55fef42c110408c7
Greg Bowler <greg.bowler@g105b.com>
2021-09-24 15:31:30 +0100

    feature: set webengine default closes #2

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -8,6 +8,14 @@ class BuildCommand extends AbstractProxyCommand {
 		$this->proxyCommand = new \Gt\Build\Cli\RunCommand();
 	}
 
+	public function run(ArgumentValueList $arguments = null):void {
+		if(!$arguments->contains("default")) {
+			$arguments->set("default", "vendor/phpgt/webengine/build.default.json");
+		}
+
+		parent::run($arguments);
+	}
+
 	public function getName():string {
 		return "build";
 	}
